f463b5be34b09311f559ec965242bdcb3d59f8b8
RaquelMarques97 <55295471+RaquelMarques97@users.noreply.github.com>
2020-03-11 19:51:11 +0000

    Update NavBar.js

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,7 +4,7 @@ import { Navbar, Nav } from 'react-bootstrap';
 function NavBar() {
     return (
         <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#home">IMPERIAL SPARK</Navbar.Brand>
+            <Navbar.Brand href="#/">IMPERIAL SPARK</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
